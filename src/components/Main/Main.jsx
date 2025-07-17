e8b010650b65a29ed08e525dbe0e5d57b1a8b072
Іван <65630800+bahioiiia@users.noreply.github.com>
2025-07-17 18:16:57 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,28 +7,24 @@ const Main = ({ children, title = 'Main Content' }) => {
     <section className={s.content}>
       <h1>{title}</h1>
       {children}
+      <p>Current count: </p>
+
+<h1>Welcome to the Keyboard Fan Screen App</h1>
+<p>Here you can find information about your favorite peripherals.</p>
+<p>Click the button to increase the count.</p>
+<p>Current count: </p>
+<h1>Welcome to the Keyboard Fan Screen App</h1>
+<p>Here you can find information about your favorite peripherals.</p>
+<p>Click the button to increase the count.</p>
+<p>Current count: </p>
+<h1>Welcome to the Keyboard Fan Screen App</h1>
+<p>Here you can find information about your favorite peripherals.</p>
+<p>Click the button to increase the count.</p>
+<p>Current count: </p>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita quisquam labore maiores reprehenderit ullam consequuntur, cupiditate optio incidunt id, iusto quae. Nostrum eum facere obcaecati sunt saepe perferendis inventore.
+
     </section>
   );
 };
 
 export default Main;
-//     <p>Current count: </p>
-
-//     <h1>Welcome to the Keyboard Fan Screen App</h1>
-//     <p>Here you can find information about your favorite peripherals.</p>
-//     <p>Click the button to increase the count.</p>
-//     <p>Current count: </p>
-//     <h1>Welcome to the Keyboard Fan Screen App</h1>
-//     <p>Here you can find information about your favorite peripherals.</p>
-//     <p>Click the button to increase the count.</p>
-//     <p>Current count: </p>
-//     <h1>Welcome to the Keyboard Fan Screen App</h1>
-//     <p>Here you can find information about your favorite peripherals.</p>
-//     <p>Click the button to increase the count.</p>
-//     <p>Current count: </p>
-//     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita quisquam labore maiores reprehenderit ullam consequuntur, cupiditate optio incidunt id, iusto quae. Nostrum eum facere obcaecati sunt saepe perferendis inventore.
-//   </section>
-//   );
-// };
-
-// export default Main;
